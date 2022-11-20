7fcf319ec516ad08aa940a5b32a5d08d5f995d65
agomezg1822 <115677803+agomezg1822@users.noreply.github.com>
2022-11-20 14:31:17 -0500

    Programa principal
    Se agrego la libreria telegram y time, el programa toma fotos y las envia cuando se active la deteccion de movimiento

--- video.py
+++ video.py
@@ -3,12 +3,19 @@ import numpy as np
 import imutils
 import winsound
 import requests
+import telegram
+import time
+
 
 #https://api.telegram.org/
 #https://core.telegram.org/bots/api
 
+#telegram
+bot_token = 'token'
+chat_id='el @ de su canal publico'
+bot = telegram.Bot(token=bot_token)
 
-video = cv2.VideoCapture('http://192.168.20.110:81/stream')#la url de la esp32
+video = cv2.VideoCapture('direccion ip de la camara')#la url de la esp32
 
 i = 0
 while True:
@@ -26,13 +33,17 @@ while True:
       if area > 10000:
         x,y,w,h = cv2.boundingRect(c)
         cv2.rectangle(frame, (x,y), (x+w,y+h),(0,255,0),2)
-        requests.post('https://api.telegram.org/bot"aca pone el token de su bot"/sendMessage',
-         data = {'chat_id': "aca pone el @ de su canal publico", 'text': "texto que quiere enviar"}) #envia al api de telgram
-        winsound.PlaySound("C:/datos disco/CURSOS/PROYECTOS/grabar/alarma2.wav",  winsound.SND_ASYNC ) #reproducir el sonido
+        winsound.PlaySound("C:/datos disco/CURSOS/PROYECTOS/grabar/alarma2.wav",  winsound.SND_ASYNC ) #reproducir el sonidO
+       #enviar foto 
+        with open ('C:/datos disco/CURSOS/PROYECTOS/grabar/fotos/imagen.jpg','rb') as photo_file: 
+          bot.sendPhoto(chat_id=chat_id,photo=photo_file, caption='ALERTA INTRUSO DETECTADO') #mensaje que aparecera con la foto
+
+        
        
-  
+  cv2.imwrite('C:/datos disco/CURSOS/PROYECTOS/grabar/fotos/imagen.jpg',frame)
   cv2.imshow('Frame',frame)
   i = i+1
   if cv2.waitKey(30) & 0xFF == 27: #cuando presione 'Esc' se va a cerrar el programa
     break
 video.release()
+
